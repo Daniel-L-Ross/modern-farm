d5cd1c88cc8d53041e77afcd40af61167fc63e8c
Dan Ross <daniellross@gmail.com>
2021-01-21 13:45:29 -0600

    added field js, started on if else statement for handling exception

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,34 @@
 import { createPlan } from './plan.js'
 
 // temporary code to test seeds
-import { createAsparagus } from './seeds/asparagus.js'
-import { createCorn } from './seeds/corn.js'
-import { createPotato } from './seeds/potato.js'
-import { createSoybean } from './seeds/soybean.js'
-import { createSunflower } from './seeds/sunflower.js'
-import { createWheat } from './seeds/wheat.js'
 
-const asparagusSeed = createAsparagus()
-console.log(asparagusSeed)
+// import { createAsparagus } from './seeds/asparagus.js'
+// import { createCorn } from './seeds/corn.js'
+// import { createPotato } from './seeds/potato.js'
+// import { createSoybean } from './seeds/soybean.js'
+// import { createSunflower } from './seeds/sunflower.js'
+// import { createWheat } from './seeds/wheat.js'
 
-const cornSeed = createCorn()
-console.log(cornSeed)
+// const asparagusSeed = createAsparagus()
+// console.log(asparagusSeed)
 
-const potatoSeed = createPotato()
-console.log(potatoSeed)
+// const cornSeed = createCorn()
+// console.log(cornSeed)
 
-const soybeanSeed = createSoybean()
-console.log(soybeanSeed)
+// const potatoSeed = createPotato()
+// console.log(potatoSeed)
 
-const sunflowerSeed = createSunflower()
-console.log(sunflowerSeed)
+// const soybeanSeed = createSoybean()
+// console.log(soybeanSeed)
 
-const wheatSeed = createWheat()
-console.log(wheatSeed)
+// const sunflowerSeed = createSunflower()
+// console.log(sunflowerSeed)
+
+// const wheatSeed = createWheat()
+// console.log(wheatSeed)
+
+// const yearlyPlan = createPlan ()
+// console.log(yearlyPlan)
 
 // end of test code section
 console.log("Welcome to the main module")
-
-const yearlyPlan = createPlan ()
-console.log(yearlyPlan)
